ed3de1b99d8691bab7f294525deb574e1ce4280f
Josh Stoik <josh@blobfolio.com>
2020-11-07 12:37:56 -0800

    build: 0.1.0

---
+++ release/bash_completion.d/cargo-bashman.bash
@@ -0,0 +1,40 @@
+_basher___cargo_bashman() {
+	local cur prev opts
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	opts=()
+		
+	if [[ ! " ${COMP_LINE} " =~ " -h " ]] && [[ ! " ${COMP_LINE} " =~ " --help " ]]; then
+		opts+=("-h")
+		opts+=("--help")
+	fi
+	if [[ ! " ${COMP_LINE} " =~ " -V " ]] && [[ ! " ${COMP_LINE} " =~ " --version " ]]; then
+		opts+=("-V")
+		opts+=("--version")
+	fi
+	if [[ ! " ${COMP_LINE} " =~ " -m " ]] && [[ ! " ${COMP_LINE} " =~ " --manifest-path " ]]; then
+		opts+=("-m")
+		opts+=("--manifest-path")
+	fi
+
+	opts=" ${opts[@]} "
+	if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+		COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+		return 0
+	fi
+
+	case "${prev}" in
+		-m|--manifest-path)
+			COMPREPLY=( $( compgen -f "${cur}" ) )
+			return 0
+			;;
+		*)
+			COMPREPLY=()
+			;;
+	esac
+
+	COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+	return 0
+}
+complete -F _basher___cargo_bashman -o bashdefault -o default cargo-bashman
