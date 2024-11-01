8e863367b2a6f9eb2b76cfbd71f1eccbf368a229
Josh Stoik <josh@blobfolio.com>
2024-10-31 20:18:43 -0700

    docs

--- src/err.rs
+++ src/err.rs
@@ -31,13 +31,13 @@ USAGE:
     cargo bashman [FLAGS] [OPTIONS]
 
 FLAGS:
-    -h, --help                  Prints help information.
+    -h, --help                  Print help information to STDOUT and exit.
         --no-bash               Do not generate BASH completions.
         --no-credits            Do not generate CREDITS.md.
         --no-man                Do not generate MAN page(s).
         --print-targets         Print the supported target triples (for use
                                 with -t/--target) to STDOUT and exit.
-    -V, --version               Prints version information.
+    -V, --version               Print version information to STDOUT and exit.
 
 OPTIONS:
     -m, --manifest-path <FILE>  Read file paths from this list.
